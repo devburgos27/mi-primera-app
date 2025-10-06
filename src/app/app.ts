9e35599b18c9303d6c7c22d44fae71ac17683207
Benjamín <dev.burgos27@gmail.com>
2025-10-06 20:57:39 -0300

    Modificacion componente principal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('mi-primera-app');
+  title = '¡Hola Mundo con Angular 20!';
+  mensaje = 'Mi primera aplicación Angular';
+  contador = 0;
+
+  cambiarMensaje() {
+    this.mensaje = this.mensaje === 'Mi primera aplicación Angular'
+    ? '¡Angular 20 es genial!'
+    : 'Mi primera aplicación Angular'
+    this.contador++;
+  }
 }
