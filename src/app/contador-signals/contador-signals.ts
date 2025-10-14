a0a442a88bc31743fdfb33b8996bf81a8189c071
Benjamín <dev.burgos27@gmail.com>
2025-10-14 18:38:18 -0300

    creacion componente contador con signals

---
+++ src/app/contador-signals/contador-signals.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contador-signals',
+  imports: [],
+  templateUrl: './contador-signals.html',
+  styleUrl: './contador-signals.css'
+})
+export class ContadorSignals {
+
+}
