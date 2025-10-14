7f3008927b1f07f4cd435ddfcb31f2e39cadd24f
Benjamín <dev.burgos27@gmail.com>
2025-10-14 18:24:58 -0300

    uso de componente en app principal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ContadorComponent } from './contador/contador';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [],
+  imports: [ContadorComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -11,11 +11,16 @@ export class App {
   title = '¡Hola Mundo con Angular 20!';
   mensaje = 'Mi primera aplicación Angular';
   contador = 0;
+  totalClicks = 0;
 
   cambiarMensaje() {
-    this.mensaje = this.mensaje === 'Mi primera aplicación Angular'
-    ? '¡Angular 20 es genial!'
-    : 'Mi primera aplicación Angular'
+    this.mensaje = this.mensaje === 'Mi primera aplicación Angular' 
+      ? '¡Angular 20 es genial!' 
+      : 'Mi primera aplicación Angular';
     this.contador++;
+  }
+  onContadorCambio(nuevoValor: number) {
+    this.totalClicks++;
+    console.log('Contador cambió a:', nuevoValor);
   }
 }
